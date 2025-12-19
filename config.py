1ce1a65d33080f5dec39971447725a04f1b78f59
Bvogel4 <Bvogel4@gmu.edu>
2025-12-19 11:58:20 -0500

    add config file for marvel+DCJL

---
+++ config.py
@@ -0,0 +1,6 @@
+#place all sim and db specific folders here so the rest of the code doesn't need hardcoded definitions
+db_connection = '/home/bk639/data_base/CDM_all.db'
+#system path for mytangosproperties.py
+sys_path = '/home/bk639/mytangosproperty'
+
+xu_output_dir = '/home/bk639/OverLeaf/InferencePaper/CorrelationTesting_M_DC/'
