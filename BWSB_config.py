e4a5999a72fe3a7e3020549067afd0b3ecb7d209
Bvogel4 <Bvogel4@gmu.edu>
2026-05-12 14:57:57 -0400

    updates

---
+++ BWSB_config.py
@@ -0,0 +1,10 @@
+#place all sim and db specific folders here so the rest of the code doesn't need hardcoded definitions
+db_connection = '/home/bk639/data_base/BW-SB_comparison.db'
+#system path for mytangosproperties.py
+sys_path = '/home/bk639/mytangosproperty'
+pickle_file = 'ellipse_data_v_stars_BW-SB.pickle'
+ba_s_key = 'ba_s'
+ca_s_key = 'ca_s'
+
+#xu_output_dir = '/home/bk639/OverLeaf/InferencePaper/CorrelationTesting_M_DC/'
+results_output_directory = '/home/bk639/MCMC-deprojection/BW-SB.results/'
